1b00b3ca3b1352972438b2742d39cc0510b1d271
Calvin <calvinkioko91@gmail.com>
2022-06-07 13:36:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const tutorials = [
-  'what does the this keyword mean?',
+  "what does the this keyword mean?",
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial) => {
+    let words = tutorial.split(' ');
+    return words.map((word) => `${word[0].toUpperCase()}${word.slice(1)}`).join(' ')
+  })
 }
